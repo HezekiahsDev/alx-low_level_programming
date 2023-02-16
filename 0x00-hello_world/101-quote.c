d6071652929ff13cad017e9411d28562f7887983
Tychool <ok.hezekiahs@gmail.com>
2023-02-16 05:42:22 -0800

    quote erro

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,5 +9,6 @@
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	/* when using write we must specify the sring length at the end*/
 	return (1);
 }
