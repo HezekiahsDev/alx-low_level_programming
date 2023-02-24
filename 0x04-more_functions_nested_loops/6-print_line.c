cc709509e1c399a44bfb0bef9bbea5642cde3be7
Tychool <ok.hezekiahs@gmail.com>
2023-02-23 23:57:46 -0800

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,25 +6,18 @@
  */
 void print_line(int n)
 {
+	int i;
 
-
-int i;
-
-if (n > 0)
-{
-for (i = 0; i < n; i++)
-{
-_putchar (95);
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar (95);
+		}
+		_putchar ('\n');
+	}
+	else
+	{
+		_putchar ('\n');
+	}
 }
-
-_putchar ('\n');
-}
-
-else
-{
-_putchar ('\n');
-}
-
-}
-
-
