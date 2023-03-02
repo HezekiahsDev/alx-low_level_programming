d2c3d427db77e62ddf9a96c4303e9b1f24ccb871
Tychool <ok.hezekiahs@gmail.com>
2023-03-02 02:43:47 -0800

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,15 @@ void print_rev(char *s)
 {
 	int index;
 
+	/* Set loop to list all the items in the string literal */
 	for (index = 0; s[index] != '\0'; index++)
 		;
+	/* Set loop to read the strings in reverse starting from */
+	/*starting from the last character excluding the null*/
+	/*Split te strings into individual character*/
 	for (index = index - 1; s[index] != '\0'; index--)
 	{
+	/*Print individual character with _putchar*/
 		_putchar(s[index]);
 	}
 	_putchar('\n');
