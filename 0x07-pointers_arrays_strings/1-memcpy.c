87220fe386036872ed35dd708802842e50efd4c0
Tychool <ok.hezekiahs@gmail.com>
2023-03-06 01:39:16 -0800

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memory area
+ * @dest: where to copy into
+ * @src: where to copy from
+ * @n: number of bytes to copy
+ *
+ * Return: A pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
