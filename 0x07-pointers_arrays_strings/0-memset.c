22d3253fcb40ea30dbb1387e8ec6d7062fa22ba4
Tychool <ok.hezekiahs@gmail.com>
2023-03-06 11:47:06 -0800

    corrections

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,7 +10,7 @@
  * with the constant byte 'b'.
  *  Return: Pointer to memory area 's'
  */
-char *_memset(char *s, char b, insigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
