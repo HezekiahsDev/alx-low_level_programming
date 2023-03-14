15a5f07eee814cf55b845e535dbae522e584b431
Tychool <ok.hezekiahs@gmail.com>
2023-03-14 03:43:45 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,15 +18,19 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
+
 	arr = malloc(sizeof(char) * size);
 
 	/* checke if memory allocation was successful */
 
 	if (arr == NULL)
 		return (NULL);
+	
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
+
 	return (arr);
+
 }
