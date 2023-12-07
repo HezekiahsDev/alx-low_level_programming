2555116994e534710b505c47043ca36577ad2ca6
root-hezeiahs_PC <ok.hezekiahs@gmail.com>
2023-12-07 19:54:46 +0100

    added files

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add node at the begining of a DLL
+ * @head: head node
+ * @n: member value
+ *
+ * Return: pointer to new element
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int *n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+	if (*head != NULL)/*check validity*/
+		(*head)->prev = new_node;
+	*head = new_node;
+	return (new_node);
+}
